a113794ab1b526031b53c019eabfcd4865f341a0
Nyeok <dn.shin98@gmail.com>
2024-11-29 17:43:01 +0900

    [Add] deleteTodo() and its functionality

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Vanila TODO
+
 내가 Vanila JS로 앱을 만들어 본 적이 있던가? 없구나!
 
 <br/>
 
 ### Tasks
-- [ ] Add, Delete 구현
+
+- [x] Add, Delete 구현
 - [ ] Complete 상태구현 (feat. Vanila JS에서 상태란?)
--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-let input = document.getElementById('todo-input');
-let todoList = document.getElementById('todo-list');
+const input = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
 
 function addTodo() {
     const value = input.value.trim();
     if (value) {
         const todo = document.createElement('li');
-        todo.textContent = value;
+        todo.innerHTML = `
+            <span>${value}</span>
+            <button onclick='deleteTodo(this)'>Delete</button>
+            `;
         todoList.appendChild(todo);
         input.value = '';
     }
+}
+
+function deleteTodo(button) {
+    const todo = button.parentElement;
+    todoList.removeChild(todo);
 }
